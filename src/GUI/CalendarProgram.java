614e0e29f9b226fcd0e14e8d1a30f0dfae588a60
enochpowno <enochpuno@yahoo.com>
2019-02-10 15:09:26 +0800

    7th VER
    Fixed the packages

--- src/GUI/CalendarProgram.java
+++ src/GUI/CalendarProgram.java
@@ -13,8 +13,6 @@ import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.table.*;
 
-import designchallenge1.TableRenderer;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
--- src/GUI/TableRenderer.java
+++ src/GUI/TableRenderer.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package designchallenge1;
+package GUI;
 
 import java.awt.Color;
 import java.awt.Component;
